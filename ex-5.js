af93d516de593add4ed0ec05cff827f741a45876
Thanutham Baikwang <juice159@Thanuthams-MacBook-Air.local>
2024-08-16 22:09:11 +0700

    feat: answer javasript-primitive-values excercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000;
+let discount = `${25 / 100 * productPrice}`;
+console.log(discount);
+let totalPrice = productPrice - discount;
+console.log(totalPrice)
